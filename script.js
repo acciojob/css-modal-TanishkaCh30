9e40b27e47db7315b0e1da3ed83ec2cc0bc678c9
TanishkaCh30 <137798637+TanishkaCh30@users.noreply.github.com>
2025-02-08 16:33:30 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-//your JS code here. If required.
+let openButton = document.getElementById("openModal");
+let closeButton =  document.getElementById("close");
+let firstDiv =  document.getElementsByClassName("modal")[0];
+
+function openModal(){
+	firstDiv.classList.add('active');
+}
+
+function closeModal() {
+    firstDiv.classList.remove('active');
+}
+
+
+openButton.addEventListener("click" , openModal);
+closeButton.addEventListener("click", closeModal);
